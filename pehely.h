cae644f38dc01de9416e0e66172b4b500bf8da81
Andrssss <132691684+Andrssss@users.noreply.github.com>
2023-05-08 18:38:40 +0200

    Add files via upload

---
+++ pehely.h
@@ -0,0 +1,31 @@
+#ifndef PEHELY_H_INCLUDED
+#define PEHELY_H_INCLUDED
+#include <iostream>
+
+struct pehely
+{
+public : /// hogy át tudjuk alakítani széppé
+    int x,y, _XX,_YY;
+    int r, g, b;
+    std::string s;
+    pehely(int rr,int gg,int bb,std::string ss,int XX,int YY)
+    {
+        _XX = XX;
+        _YY = YY;
+        r=rr;
+        g=gg;
+        b=bb;
+        s=ss;
+        x = rand()%XX;
+        y = rand()%YY;
+        ///std::cout << x<< std::endl;
+    }
+
+
+    void mozog();
+    void rajzol();
+    bool bent();
+};
+
+#endif // PEHELY_H_INCLUDED
+
